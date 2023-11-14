6a39899f08d8020e18ee5f28ae4e9332dc80fc6a
Yann Forget <yannforget@mailbox.org>
2023-11-14 09:08:31 +0100

    add compute indicators pipeline

--- compute_indicators/pipeline.py
+++ compute_indicators/pipeline.py
@@ -1,38 +1,56 @@
-from openhexa.sdk import current_run, pipeline, parameter
+import os
+
+import papermill as pm
+from openhexa.sdk import current_run, pipeline, workspace, parameter
 
 
-@pipeline("compute-indicators", name="Compute Indicators")
+@pipeline("compute-indicators", name="Calculer les indicateurs")
 @parameter(
-    "include_kobo",
-    name="Include KoboToolbox data",
-    help="Include data from KoboToolbox surveys",
-    type=bool,
-    default=True,
-)
-@parameter(
-    "include_excel",
-    name="Include Excel data",
-    help="Include survey data from Excel sheets",
-    type=bool,
-    default=True,
-)
-@parameter(
-    "include_praps1",
-    name="Include PRAPS 1 data",
-    help="Include data from PRAPS 1",
+    "update",
+    name="Update dashboards",
+    help="Push extracted surveys to the dashboard database",
     type=bool,
     default=False,
 )
-@parameter(
-    "postgres_table",
-    name="Database table",
-    help="Target table in the workspace database",
-    type=str,
-    required=False,
-)
-def compute_indicators(include_kobo, include_excel, include_praps1, postgres_table):
-    current_run.log_info("Consolidating source data...")
+def compute_indicators(update: bool):
+    run_notebook(update)
+
+
+@compute_indicators.task
+def run_notebook(update: bool):
     current_run.log_info("Computing indicators...")
+    pm.execute_notebook(
+        input_path=os.path.join(
+            workspace.files_path,
+            "pipelines",
+            "extract-transform-load",
+            "compute-indicators.ipynb",
+        ),
+        output_path=os.path.join(
+            workspace.files_path,
+            "pipelines",
+            "extract-transform-load",
+            "data",
+            "output",
+            "notebooks",
+            "output.ipynb",
+        ),
+        parameters={"update_dashboard_database": update},
+        request_save_on_cell_execute=False,
+        progress_bar=False,
+    )
+    current_run.log_info("Finished computing indicators")
+
+    output_dir = os.path.join(
+        workspace.files_path, "pipelines", "extract-transform-load", "data", "output"
+    )
+    for fname in os.listdir(output_dir):
+        if (
+            fname.startswith("IRI-")
+            or fname.startswith("IR-")
+            or fname.startswith("Reg")
+        ) and fname.endswith(".csv"):
+            current_run.add_file_output(os.path.join(output_dir, fname))
 
 
 if __name__ == "__main__":
